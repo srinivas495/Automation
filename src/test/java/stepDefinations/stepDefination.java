8b1c8753eb36438cce654ab36a3f054eac33954f
Srinivas <srinivaschari.y@gmail.com>
2021-02-08 20:31:57 +0530

    Automation

---
+++ src/test/java/stepDefinations/stepDefination.java
@@ -0,0 +1,38 @@
+package stepDefinations;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class stepDefination {
+
+	@Given("^User is on Netbacking landing page$")
+	public void user_is_on_Netbacking_landing_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user login into application with username and password$")
+	public void user_login_into_application_with_username_and_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Home page is populated$")
+	public void home_page_is_populated() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Cards are displyed$")
+	public void cards_are_displyed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
---
+++ target/test-classes/features/Login.feature
@@ -0,0 +1,9 @@
+Feature: Application Login
+
+Scenario: Home page defalt login
+Given User is on Netbacking landing page
+When user login into application with username and password
+Then Home page is populated
+And Cards are displyed
+ 
+
